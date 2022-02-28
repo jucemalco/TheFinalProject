4388f23b23b60f4b269f1cb3cc6052855eb9641c
jucemalco <jucemalco@gmail.com>
2022-02-28 00:02:03 +0000

    first update initial set up project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './styles/App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+     <Router>
+       <Routes>
+        {/*<Route path='/' element={}/>*/}
+       </Routes>
+     </Router>
+    </div>
+  );
+}
+
+export default App;
