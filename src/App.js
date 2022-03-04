c8b7fc613ac6dcc5e663479ef16ba7961d0a52db
jucemalco <jucemalco@gmail.com>
2022-03-04 21:17:04 +0000

    basic structure final project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './styles/App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './views/Home';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
      <Router>
        <Routes>
-        {/*<Route path='/' element={}/>*/}
+        <Route path='/' element={<Home />}/>
+
        </Routes>
+      <Footer />
      </Router>
     </div>
     
