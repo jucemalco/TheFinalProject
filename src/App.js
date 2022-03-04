5e63c2dcb65c3569dc30d33164d15dccad5eb2dd
jucemalco <jucemalco@gmail.com>
2022-03-04 22:50:10 +0000

    footer fixed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './views/Home';
 import Footer from './components/footer';
 
-
 function App() {
   return (
     <div className="App">
