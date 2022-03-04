88c81161fa4980ff8667a27e2afda1cefb1a91ea
jucemalco <jucemalco@gmail.com>
2022-03-04 21:31:40 +0000

    footer is setting up, it need to be finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './styles/App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './views/Home';
 
 function App() {
   return (
     <div className="App">
      <Router>
        <Routes>
-        {/*<Route path='/' element={}/>*/}
+        <Route path='/' element={<Home />}/>
        </Routes>
      </Router>
     </div>
