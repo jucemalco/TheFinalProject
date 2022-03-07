95ece19b76bd635972138f771852ce42971016a2
Monica calle <monicacalle369@gmail.com>
2022-03-06 23:15:12 -0300

    feat: footer creation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import './styles/App.css';
+import "./styles/App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './views/Home';
-import Footer from './components/footer';
+import Home from "./views/Home";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-     <Router>
-       <Routes>
-        <Route path='/' element={<Home />}/>
-       </Routes>
-      <Footer />
-     </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
-    
   );
 }
 
