a2893c84ef017c6aa83dd9535fbac50dfe00e2e9
nicolas aravena <nicolas.aravena96@gmail.com>
2022-03-08 00:05:12 -0300

    arreglo de carusel, falta cambiar imagenes

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,29 +1,35 @@
 import React from "react"
 
-const Carousel = ( ) => {
+const Carousel = () => {
     return <div className="container">
-    <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-        <div className="carousel-inner">
-            <div className="carousel-item active">
-                <img className="d-block carousel-image w-100" src="https://thumbs.dreamstime.com/b/concepto-de-libro-audio-con-libros-abiertos-taza-caf%C3%A9-y-aud%C3%ADfonos-vintage-espacio-panor%C3%A1mico-buena-copia-auriculares-o-banner-201176625.jpg" alt="First slide" />
+        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
-            <div className="carousel-item">
-                <img className="d-block carousel-image w-100" src="https://st2.depositphotos.com/39463116/46456/i/450/depositphotos_464567262-stock-photo-concept-of-progress-in-education.jpg" alt="Second slide" />
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src="https://i0.wp.com/hipertextual.com/wp-content/uploads/2020/01/hipertextual-star-wars-deseo-mas-grande-darth-vader-podria-hacerse-realidad-muy-pronto-2020659163.jpg?fit=2048%2C1365&ssl=1" className="d-block carousel-imag w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="https://img.europapress.es/fotoweb/fotonoticia_20181023133557_1200.jpg" className="d-block carousel-image w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fCrEZVM8x7-Cbtqxj2emWYCQZNtT3Gc6eA&usqp=CAU" className="d-block carousel-imag w-100" alt="..."/>
+                </div>
             </div>
-            <div className="carousel-item">
-                <img className="d-block carousel-image w-100" src="https://picsum.photos/id/237/200/300" alt="Third slide" />
-            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
         </div>
-        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="sr-only">Previous</span>
-        </a>
-        <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="sr-only">Next</span>
-        </a>
     </div>
-    </div>
+
 }
 
 export default Carousel;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import './styles/index.css';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
 
 
