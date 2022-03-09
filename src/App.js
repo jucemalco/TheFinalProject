9f0eb18c2ff0dba76c0f7047f098e1ec5cd46806
JulioM <jucemalco@gmail.com>
2022-03-09 19:53:33 -0300

    navbar update with whole the links

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './styles/App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './views/Home';
 import Footer from './components/footer';
+import NavBar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
      <Router>
+       <NavBar />
        <Routes>
         <Route path='/' element={<Home />}/>
        </Routes>
