0d863e5128c4aae30a30264c99ae44ce11d76565
Monica calle <monicacalle369@gmail.com>
2022-03-09 20:56:24 -0300

    feat:match status creation

---
+++ src/components/RoundedCard/RoundedCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+const RoundedCard = (props) => {
+  return (
+    <div className=" card d-flex justify-content-center align-items-center">
+      <img
+        className="rounded-card--profile_picture rounded-circle"
+        src="https://www.softzone.es/app/uploads/2018/04/guest.png?x=480&quality=20"
+        alt="user profile picture"
+      />
+      <div className="card-body">
+        <p className="card-text">{props.nombre}</p>
+      </div>
+    </div>
+  );
+};
+
+export default RoundedCard;
---
+++ src/views/MatchStatus.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { StatusError } from "../components/StatusError";
+import { RoundedCard } from "../components/RoundedCard";
+import SquareCard from "../components/SquareCard/SquareCard";
+import "./MatchStatus.css";
+
+const MatchStatus = () => {
+  return (
+    <div className="d-flex align-items-center justify-content-around">
+      <div className="d-flex align-items-center justify-content-around flex-column">
+        <RoundedCard nombre="juan martinez" />
+        <SquareCard nombre="Rayuela" />
+      </div>
+
+      <StatusError />
+
+      <div className="d-flex align-items-center justify-content-around flex-column">
+        <RoundedCard nombre="pepita perez" />
+        <SquareCard nombre="Rayuela" />
+      </div>
+    </div>
+  );
+};
+
+export default MatchStatus;
