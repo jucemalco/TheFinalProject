ad37f5b2517be9ec7c7383b5b8f07aebe5c19ec7
Monica calle <monicacalle369@gmail.com>
2022-03-11 21:06:40 -0300

    feature/popper-update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import "./styles/App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './views/Home';
-import Footer from './components/footer';
-import injectContext from "./store/appContext"
-import Product from './views/Product';
-import NavBar from './components/navbar';
-import Formsignin from './components/formsignin';
-import { Component } from 'react';
+import Home from "./views/Home";
+import Footer from "./components/footer";
+import injectContext from "./store/appContext";
+import Product from "./views/Product";
+import NavBar from "./components/navbar";
+import Formsignin from "./components/formsignin";
+import { Component } from "react";
+import MatchStatus from "./views/MatchStatus";
 
 function App() {
   return (
     <div className="App">
-     <Router>
-       <NavBar />
-       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/login/formsignin' element={<Formsignin />} />
-        <Route path="/match-status" element={<MatchStatus />} />
-         <Route path='/admin/Product' element={<Product />}/>
-       </Routes>
-      <Footer />
-     </Router>
-
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/formsignin" element={<Formsignin />} />
+          <Route path="/match-status" element={<MatchStatus />} />
+          <Route path="/admin/Product" element={<Product />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
