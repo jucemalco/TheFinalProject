c000c19dcac337d51d6dca62fc446fb2f1e1e792
Monica calle <monicacalle369@gmail.com>
2022-03-22 15:45:42 -0300

    fix/footer details

--- src/views/UserProfile/UserProfile.js
+++ src/views/UserProfile/UserProfile.js
@@ -6,7 +6,9 @@ import ProfileNavbar from "../../components/ProfileNavbar/ProfileNavbar";
 
 const UserProfile = (props) => {
   return (
-    <> <ProfileNavbar />
+    <>
+      {" "}
+      <ProfileNavbar />
       <section className="h-100 gradient-custom-2">
         <div className="container py-5 h-100">
           <div className="row d-flex justify-content-center align-items-center h-100">
@@ -60,120 +62,108 @@ const UserProfile = (props) => {
                   </div>
                 </div>
                 <div className="card-body p-4 text-black">
-                  <div className="mb-5">
-                    <p className="lead fw-normal mb-1">About</p>
-                    <div className="p-4" style={{ backgroundColor: "#f8f9fa" }}>
-                      <p className="font-italic mb-1">Web Developer</p>
+                  <div className="ms-3">
+                    <h5>{props.name}</h5>
+                    <p>{props.city}</p>
+                  </div>
+                </div>
+                <div
+                  className="p-4 text-black"
+                  style={{ backgroundColor: "#f8f9fa" }}
+                >
+                  <div className="d-flex justify-content-end text-center py-1 ">
+                    <div class="dropdown">
+                      <button
+                        class="btn btn-secondary dropdown-toggle"
+                        type="button"
+                        id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        otras opciones
+                      </button>
+                      <ul
+                        class="dropdown-menu"
+                        aria-labelledby="dropdownMenuButton1"
+                      >
+                        <li>
+                          <a class="dropdown-item" href="#">
+                            Historial de matches
+                          </a>
+                        </li>
+                        <li>
+                          <a class="dropdown-item" href="#">
+                            Mis matches pendientes
+                          </a>
+                        </li>
+                        <li>
+                          <a class="dropdown-item" href="#">
+                            Nueva solicitud de match
+                          </a>
+                        </li>
+                      </ul>
                     </div>
-                    Editar perfil
-                  </button>
-                </div>
-                <div className="ms-3">
-                  <h5>{props.name}</h5>
-                  <p>{props.city}</p>
-                </div>
-              </div>
-              <div
-                className="p-4 text-black"
-                style={{ backgroundColor: "#f8f9fa" }}
-              >
-                <div className="d-flex justify-content-end text-center py-1 ">
-                  <div class="dropdown">
-                    <button
-                      class="btn btn-secondary dropdown-toggle"
-                      type="button"
-                      id="dropdownMenuButton1"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      otras opciones
-                    </button>
-                    <ul
-                      class="dropdown-menu"
-                      aria-labelledby="dropdownMenuButton1"
-                    >
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          Historial de matches
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          Mis matches pendientes
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          Nueva solicitud de match
-                        </a>
-                      </li>
-                    </ul>
                   </div>
                 </div>
-              </div>
-              <div className="card-body p-4 text-black">
-                <div className="mb-5">
-                  <p className="lead fw-normal mb-1">sobre mi</p>
-                  <div className="p-4" style={{ backgroundColor: "#f8f9fa" }}>
-                    <p className="font-italic mb-1">Web Developer</p>
-                  </div>
-                </div>
-                <div className="d-flex justify-content-between align-items-center mb-4">
-                  <p className="lead fw-normal mb-0">Mis libros</p>
-                  <p className="mb-0">
-                    <Link to="#!" className="text-muted">
-                      Mostrar todos
-                    </Link>
-                  </p>
-                </div>
-                <div className="row g-2">
-                  <div className="col mb-2">
-                    <Card />
-                  </div>
-                  <div className="col mb-2">
-                    <Card />
-                  </div>
-                </div>
-                <div className="row g-2">
-                  <div className="col">
-                    <Card />
-                  </div>
-                  <div className="col">
-                    <Card />
+                <div className="card-body p-4 text-black">
                   <div className="d-flex justify-content-between align-items-center mb-4">
-                    <p className="lead fw-normal mb-0">Libros Recientes</p>
+                    <p className="lead fw-normal mb-0">Mis libros</p>
                     <p className="mb-0">
                       <Link to="#!" className="text-muted">
-                        Mostrar Todo
+                        Mostrar todos
                       </Link>
                     </p>
                   </div>
                   <div className="row g-2">
-                    <div className="col mb-2 mt-5">
-                      <h1>Mis Libros Publicados</h1>
+                    <div className="col mb-2">
+                      <Card />
                     </div>
                     <div className="col mb-2">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(107).webp"
-                        alt="image 2"
-                        className="w-100 rounded-3"
-                      />
+                      <Card />
                     </div>
                   </div>
                   <div className="row g-2">
                     <div className="col">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(108).webp"
-                        alt="image 3"
-                        className="w-100 rounded-3"
-                      />
+                      <Card />
                     </div>
                     <div className="col">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(114).webp"
-                        alt="image 4"
-                        className="w-100 rounded-3"
-                      />
+                      <Card />
+                      <div className="d-flex justify-content-between align-items-center mb-4">
+                        <p className="lead fw-normal mb-0">Libros Recientes</p>
+                        <p className="mb-0">
+                          <Link to="#!" className="text-muted">
+                            Mostrar Todo
+                          </Link>
+                        </p>
+                      </div>
+                      <div className="row g-2">
+                        <div className="col mb-2 mt-5">
+                          <h1>Mis Libros Publicados</h1>
+                        </div>
+                        <div className="col mb-2">
+                          <img
+                            src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(107).webp"
+                            alt="image 2"
+                            className="w-100 rounded-3"
+                          />
+                        </div>
+                      </div>
+                      <div className="row g-2">
+                        <div className="col">
+                          <img
+                            src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(108).webp"
+                            alt="image 3"
+                            className="w-100 rounded-3"
+                          />
+                        </div>
+                        <div className="col">
+                          <img
+                            src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(114).webp"
+                            alt="image 4"
+                            className="w-100 rounded-3"
+                          />
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -182,7 +172,8 @@ const UserProfile = (props) => {
           </div>
         </div>
       </section>
-    </>);
+    </>
+  );
 };
 
 export default UserProfile;
